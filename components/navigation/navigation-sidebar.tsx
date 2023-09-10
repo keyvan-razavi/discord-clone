f9d85b1bd3e28c530b687d13c8f275453bed1672
keyvan <keyvan.razavi20@gmail.com>
2023-09-10 10:23:13 +0330

    test

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -20,7 +20,7 @@ export const NavigationSidebar = async () => {
   });
 
   return (
-    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+    <div className="space-y-4 flex align-middle flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationAction />
     </div>
   );
