231034b70b19f9843875cc1ece281393a718b66a
keyvan <key7an@my-mac.local>
2023-09-10 10:15:19 +0330

    adding separator and tooltip

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,33 @@
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+interface ActionTooltipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+export const ActionTooltip = ({
+  label,
+  children,
+  align,
+  side,
+}: ActionTooltipProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import { NavigationAction } from "./navigation-action";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -20,7 +21,7 @@ export const NavigationSidebar = async () => {
 
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-      SIDEBAR
+      <NavigationAction />
     </div>
   );
 };
