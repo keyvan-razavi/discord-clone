baa1c395ffd295ce51176d5a2ee962be336da153
keyvan <keyvan.razavi20@gmail.com>
2023-09-21 14:50:29 +0330

    editing text color of server header

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,12 +1,13 @@
 "use client";
 
+import { ServerWithMemberWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import {
   ChevronDown,
   LogOut,
   PlusCircle,
   Settings,
-  TrashIcon,
+  Trash,
   UserPlus,
   Users,
 } from "lucide-react";
@@ -18,7 +19,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ServerWithMemberWithProfiles } from "@/types";
 import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
@@ -28,22 +28,23 @@ interface ServerHeaderProps {
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
   const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none" asChild>
-        <button className=" w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 border-b-2 dark:border-neutral-800 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition ">
+        <button className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
           {server.name}
           <ChevronDown className="h-5 w-5 ml-auto" />
         </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className=" w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
+      <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
           <DropdownMenuItem
             onClick={() => onOpen("invite", { server })}
-            className="text-indigo-600 dark:text-indigo-400 px-2 py-2 text-sm cursor-pointer"
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
           >
             Invite People
             <UserPlus className="h-4 w-4 ml-auto" />
@@ -52,16 +53,16 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isAdmin && (
           <DropdownMenuItem
             onClick={() => onOpen("editServer", { server })}
-            className=" dark:text-indigo-400 px-2 py-2 text-sm cursor-pointer"
+            className="px-3 py-2 text-sm cursor-pointer"
           >
             Server Settings
             <Settings className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
-        )}{" "}
+        )}
         {isAdmin && (
           <DropdownMenuItem
             onClick={() => onOpen("members", { server })}
-            className=" dark:text-indigo-400 px-2 py-2 text-sm cursor-pointer"
+            className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
             <Users className="h-4 w-4 ml-auto" />
@@ -70,7 +71,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isModerator && (
           <DropdownMenuItem
             onClick={() => onOpen("createChannel")}
-            className=" dark:text-indigo-400 px-2 py-2 text-sm cursor-pointer"
+            className="px-3 py-2 text-sm cursor-pointer"
           >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
@@ -80,16 +81,16 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         {isAdmin && (
           <DropdownMenuItem
             onClick={() => onOpen("deleteServer", { server })}
-            className="text-rose-500 dark:text-indigo-400 px-2 py-2 text-sm cursor-pointer"
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Delete Server
-            <TrashIcon className="h-4 w-4 ml-auto" />
+            <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
-        )}{" "}
+        )}
         {!isAdmin && (
           <DropdownMenuItem
             onClick={() => onOpen("leaveServer", { server })}
-            className="text-rose-500 dark:text-indigo-400 px-2 py-2 text-sm cursor-pointer"
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Leave Server
             <LogOut className="h-4 w-4 ml-auto" />
